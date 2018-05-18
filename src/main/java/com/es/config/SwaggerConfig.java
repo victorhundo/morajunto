1f58103fbff728878536616fa23b83b7ba051fb8
Hector Medeiros <hector.medeiros@ccc.ufcg.edu.br>
2018-05-18 14:30:20 -0300

    Ajustando Swagger

--- src/main/java/com/es/config/SwaggerConfig.java
+++ src/main/java/com/es/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
 	public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()                 .apis(RequestHandlerSelectors.basePackage("com.es.controllers"))
-                .paths(regex("/projetoes.*"))
+                .paths(regex("/api.*"))
                 .build()
         		.apiInfo(apiInfo());
              
